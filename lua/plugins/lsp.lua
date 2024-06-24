a212fa078df1ca8879a692136e66a6ffbb58dcf1
Brad Searle <bradleysearle@gmail.com>
2024-06-24 20:15:09 +1000

    lsp and term

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,26 @@
 return {
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = {
-  --     inlay_hints = {
-  --       enabled = true,
-  --     },
-  --   },
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        enabled = true,
+      },
+      servers = {
+        -- disable hint diagnostics
+        -- https://github.com/microsoft/pyright/discussions/5852#discussioncomment-6874502
+        -- pyright = {
+        --   capabilities = {
+        --     textDocument = {
+        --       publishDiagnostics = {
+        --         tagSupport = {
+        --           valueSet = { 2 },
+        --         },
+        --       },
+        --     },
+        --   },
+        -- },
+        -- ruff_lsp = {},
+      },
+    },
+  },
 }
