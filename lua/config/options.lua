e8f0d2f2815cb3b5cd8e292932ac3cf3e77c2983
Brad Searle <bradleysearle@gmail.com>
2024-06-27 20:00:52 +1000

    Code snippets

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- inlay_hints = { enabled = true },
--- relativenumber = { enabled = false },
+-- Line numbering
 vim.opt.relativenumber = false -- no line numbers
